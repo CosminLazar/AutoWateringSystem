2f7e38e4d944daf08abd832bd932563eafdbddcb
Cosmin Lazar <ccl@d60.dk>
2015-09-20 15:23:46 +0200

    Add watter pump control functions

--- WaterPump.cpp
+++ WaterPump.cpp
@@ -8,10 +8,28 @@ WaterPump::WaterPump(uint8_t powerPin, WaterSensor* waterSensor)
 {
 	this->_powerPin = powerPin;
 	this->_waterSensor = waterSensor;
+
+	pinMode(this->_powerPin, OUTPUT);
+	digitalWrite(this->_powerPin, LOW);
 }
 
 WaterPump::WaterPump(uint8_t powerPin) : WaterPump(powerPin, NULL)
 { }
+
+bool WaterPump::Start()
+{
+	if (!this->HasWater())
+		return false;
+
+	digitalWrite(this->_powerPin, HIGH);
+
+	return true;
+}
+
+void WaterPump::Stop()
+{
+	digitalWrite(this->_powerPin, LOW);
+}
 
 bool WaterPump::HasWater()
 {
--- WaterPump.h
+++ WaterPump.h
@@ -16,7 +16,8 @@ class WaterPump
 public:
 	WaterPump(uint8_t powerPin, WaterSensor* waterSensor);
 	WaterPump(uint8_t powerPin);
-
+	bool Start();
+	void Stop();
 private:
 	WaterSensor* _waterSensor;
 	uint8_t _powerPin;	
