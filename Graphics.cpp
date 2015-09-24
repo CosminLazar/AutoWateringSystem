d90c72302d885e663bf5980ff8cba28f6d82bfb6
Cosmin Lazar <ccl@d60.dk>
2015-09-24 21:22:40 +0200

    Add streamed Xbm image drawing capabilities
    Since the RAM memory of the Arduino Uno is 2 KB

--- Graphics.cpp
+++ Graphics.cpp
@@ -1,7 +1,3 @@
-// 
-// 
-// 
-
 #include "Graphics.h"
 
 Graphics::Graphics(uint8_t sdaPin, uint8_t sclPin)
@@ -10,7 +6,56 @@ Graphics::Graphics(uint8_t sdaPin, uint8_t sclPin)
 	this->displayController->Initial();
 }
 
-void Graphics::Draw_Xbm(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, const char xbm[]) {
-
+uint8_t Graphics::GetWidth()
+{
+	return SCREEN_WIDTH;
 }
 
+uint8_t Graphics::GetHeight()
+{
+	return SCREEN_HEIGHT;
+}
+
+void Graphics::Draw_Xbm(unsigned char x, unsigned char y, XbmImage & image)
+{
+	//validate y, height, width % 8
+
+	//the buffer must fit at least one full line on the screen
+	const uint8_t bufferSize = SCREEN_WIDTH;
+	uint8_t buffer[bufferSize];
+	ResourceStreamer streamer = image.GetStream();
+
+	uint8_t noOfLines = image.GetHeight() / 8;
+	uint8_t rightMostX = x + image.GetWidth();
+	uint8_t bytesPerLine = image.GetWidth();
+	uint8_t noOfLinesFittingInBuffer = bufferSize / bytesPerLine;
+
+
+	for (uint8_t sliceYPosition = 0; sliceYPosition < noOfLines; sliceYPosition += noOfLinesFittingInBuffer) {
+		uint8_t bytesRead = streamer.ReadNext(buffer, bufferSize);
+		uint8_t linesRead = bytesRead / bytesPerLine;
+
+		this->displayController->Draw_BMP(x, sliceYPosition, rightMostX, sliceYPosition + linesRead, (char *)buffer);
+	}
+}
+
+void Graphics::Print(const char text[], TextSize size, unsigned char x, unsigned char y)
+{
+	switch (size)
+	{
+	case s6x8:
+		this->displayController->Char_F6x8(x, y, text);
+		break;
+	case s8x16:
+		this->displayController->Char_F8x16(x, y, text);
+		break;
+	default:
+		this->displayController->Char_F6x8(x, y, text);
+		break;
+	}
+}
+
+void Graphics::Clear()
+{
+	this->displayController->Fill_Screen(BLACK);
+}
--- Graphics.h
+++ Graphics.h
@@ -2,7 +2,16 @@
 #ifndef _GRAPHICS_h
 #define _GRAPHICS_h
 
+#ifndef SCREEN_WIDTH
+#define SCREEN_WIDTH 128
+#endif
+
+#ifndef SCREEN_HEIGHT
+#define SCREEN_HEIGHT 64
+#endif
+
 #include <IIC_without_ACK.h>
+#include "XbmImage.h"
 
 #if defined(ARDUINO) && ARDUINO >= 100
 #include "arduino.h"
@@ -10,12 +19,16 @@
 #include "WProgram.h"
 #endif
 
+enum TextSize { s6x8, s8x16 };
 
 class Graphics {
 public:
 	Graphics(uint8_t sdaPin, uint8_t sclPin);
-	void Draw_Xbm(unsigned char x0, unsigned char y0, unsigned char x1, unsigned char y1, const char xbm[]);
-
+	uint8_t GetWidth();
+	uint8_t GetHeight();
+	void Draw_Xbm(unsigned char x0, unsigned char y0, XbmImage & image);
+	void Print(const char text[], TextSize size, unsigned char x, unsigned char y);
+	void Clear();
 private:
 	IIC_without_ACK *displayController;
 };
